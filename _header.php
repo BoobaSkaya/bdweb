7d01ec50596a3c8f15516de0fbafca907fb5f57d
Booba Skaya <booba.skaya@gmail.com>
2015-03-17 12:10:54 +0000

    Moved header and footer to separate files to be included

---
+++ _header.php
@@ -0,0 +1,41 @@
+<?php
+require_once "_tools.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta charset="utf-8">
+        <title>Mediathèque BD</title>
+        <meta name="generator" content="Bootply" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!--[if lt IE 9]>
+                <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <link href="css/styles.css" rel="stylesheet">     
+        <style>
+            body { padding-top: 50px; }
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-inverse navbar-fixed-top" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <a href="/BD2/Web/boot.php" class="navbar-brand">MEDIATHEQUE BD</a>
+                </div>
+                <nav class="collapse navbar-collapse" role="navigation">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="http://ce.palays.free.fr/BD/Web/index.php">L'ancien site</a>
+                        </li>
+                        <li>
+                            <a href="http://ce.palays.free.fr/BD/Web/moteur.php?coll=albums">Rechercher</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+
+        <!--main-->
+        <div class="container">
--- index.php
+++ index.php
@@ -1,44 +1,5 @@
-<?php
-require_once "_tools.php";
-?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8">
-        <meta charset="utf-8">
-        <title>Mediathèque BD</title>
-        <meta name="generator" content="Bootply" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <!--[if lt IE 9]>
-                <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-        <link href="css/styles.css" rel="stylesheet">     
-        <style>
-            body { padding-top: 50px; }
-        </style>
-    </head>
-    <body>
-        <nav class="navbar navbar-default navbar-inverse navbar-fixed-top" role="banner">
-            <div class="container">
-                <div class="navbar-header">
-                    <a href="/BD2/Web/boot.php" class="navbar-brand">MEDIATHEQUE BD</a>
-                </div>
-                <nav class="collapse navbar-collapse" role="navigation">
-                    <ul class="nav navbar-nav">
-                        <li>
-                            <a href="http://ce.palays.free.fr/BD/Web/index.php">L'ancien site</a>
-                        </li>
-                        <li>
-                            <a href="http://ce.palays.free.fr/BD/Web/moteur.php?coll=albums">Rechercher</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </nav>
+<?php require("_header.php"); ?>
 
-        <!--main-->
-        <div class="container">
             <div class="row">
                 <!--left-->
                 <div class="col-md-3" id="leftCol">
@@ -74,11 +35,4 @@ require_once "_tools.php";
                     <hr>
                 </div><!--/right-->
             </div><!--/row-->
-        </div><!--/container-->
-
-        <!-- script references -->
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/scripts.js"></script>
-    </body>
-</html>
+<?php require("_footer.php"); ?> 
